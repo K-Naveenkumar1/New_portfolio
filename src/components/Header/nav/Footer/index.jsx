e3de4428b07952cf0901ca9826dcea63542aa1a5
NAVEEN KUMAR K <knaveenkumar894@gmail.com>
2025-02-17 10:30:01 +0530

    update

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './style.module.scss';
+
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a href='https://www.google.com'>Github</a>
+        <a href=''>Instagram</a>
+        <a href=''>HackerRank</a>
+        <a href=''>LinkedIn</a>
+    </div>
+  )
+}
